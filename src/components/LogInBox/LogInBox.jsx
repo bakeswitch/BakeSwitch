f196919d96cce6d5c059964aa166f8b71a510762
laitzheng <laitzheng@gmail.com>
2021-05-29 09:43:05 +0800

    Add manual log in

--- src/components/LogInBox/LogInBox.jsx
+++ src/components/LogInBox/LogInBox.jsx
@@ -1,41 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./LogInBox.module.css";
 import { Button } from "@material-ui/core";
 import GoogleButton from "react-google-button";
 
 function LogInBox(props) {
-  return (
-    <div className={styles.mainBox}>
-      <h2>Log In</h2>
-      <div className={styles.googleLogIn}>
-        <GoogleButton
-          label="Log in with Google"
-          onClick={props.googleSignInFunc}
-        />
-      </div>
-      <h6>OR</h6>
-      <hr />
-      <form>
-        <div className={styles.inputBox}>
-          <input
-            id="log-in-username"
-            type="text"
-            placeholder="Username"
-            className={styles.inputText}
-          />
-          <input
-            id="log-in-password"
-            type="password"
-            placeholder="Password"
-            className={styles.inputText}
-          />
-        </div>
-        <Button variant="contained" color="primary" className={styles.button}>
-          Log In
-        </Button>
-      </form>
-    </div>
-  );
+	const [loginInput, setLoginInput] = useState({
+		email: "",
+		password: "",
+	});
+
+	function handleChange(event) {
+		const { name, value } = event.target;
+		setLoginInput((prev) => ({
+			...prev,
+			[name]: value,
+		}));
+	}
+
+	function submitChange(event) {
+		setLoginInput({
+			email: "",
+			password: "",
+		});
+		props.passwordSignIn(loginInput.email, loginInput.password);
+	}
+
+	return (
+		<div className={styles.mainBox}>
+			<h2>Log In</h2>
+			<div className={styles.googleLogIn}>
+				<GoogleButton label="Log in with Google" onClick={props.googleSignInFunc} />
+			</div>
+			<h6>OR</h6>
+			<hr />
+			<form>
+				<div className={styles.inputBox}>
+					<input
+						name="email"
+						type="email"
+						placeholder="Email"
+						onChange={handleChange}
+						value={loginInput.email}
+						className={styles.inputText}
+					/>
+					<input
+						name="password"
+						type="password"
+						placeholder="Password"
+						onChange={handleChange}
+						value={loginInput.password}
+						className={styles.inputText}
+					/>
+				</div>
+				<Button
+					variant="contained"
+					color="primary"
+					className={styles.button}
+					onClick={submitChange}
+				>
+					Log In
+				</Button>
+			</form>
+		</div>
+	);
 }
 
 export default LogInBox;
--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -3,16 +3,41 @@ import LogInBox from "../components/LogInBox";
 import { auth, GoogleAuthProvider } from "../config/firebase";
 
 export default function LogIn(props) {
-  const googleSignIn = () => {
-    auth.signInWithPopup(GoogleAuthProvider).then((result) => {
-      props.onLogIn();
-      alert("Welcome " + result.user.displayName + "!");
-    });
-  };
+	const googleSignIn = () => {
+		auth.signInWithPopup(GoogleAuthProvider).then((result) => {
+			props.onLogIn();
+			alert("Welcome " + result.user.displayName + "!");
+		});
+	};
 
-  return (
-    <>
-      <LogInBox googleSignInFunc={googleSignIn} />
-    </>
-  );
+	const passwordSignIn = (email, password) => {
+		auth
+			.signInWithEmailAndPassword(email, password)
+			.then((result) => {
+				props.onLogIn();
+				alert("Welcome " + result.user.displayName + "!");
+			})
+			.catch((error) => {
+				const errorCode = error.code;
+				switch (errorCode) {
+					case "auth/invalid-email":
+						alert("Please enter a valid email");
+						break;
+					case "auth/user-not-found":
+						alert("User not found. Please sign up for an account.");
+						break;
+					case "auth/wrong-password":
+						alert("Invalid password");
+						break;
+					default:
+						alert(error.message);
+				}
+			});
+	};
+
+	return (
+		<>
+			<LogInBox googleSignInFunc={googleSignIn} passwordSignIn={passwordSignIn} />
+		</>
+	);
 }
