d24d365f866f4ed1a3fbef01d8330ec950237d6d
laitzheng <laitzheng@gmail.com>
2021-06-12 00:56:10 +0800

    Enable signing up as seller in ManualSignUp

--- src/components/ManualSignUp/ManualSignUp.jsx
+++ src/components/ManualSignUp/ManualSignUp.jsx
@@ -6,15 +6,22 @@ import { useHistory } from "react-router-dom";
 import { db } from "../../config/firebase";
 
 const ManualSignUp = () => {
+	const { signUp, logOut } = useAuth();
+	const history = useHistory();
+
 	const usernameRef = useRef();
 	const passwordRef = useRef();
 	const passwordConfirmRef = useRef();
 	const phoneRef = useRef();
 	const emailRef = useRef();
-	const { signUp, logOut } = useAuth();
+	const shopNameRef = useRef();
+	const postalCodeRef = useRef();
+	const addressRef = useRef();
+
 	const [errors, setErrors] = useState("");
 	const [loading, setLoading] = useState(false);
-	const history = useHistory();
+	const [yesSeller, setYesSeller] = useState(false);
+	const [showSubmit, setShowSubmit] = useState(false);
 
 	async function handleSubmit(e) {
 		e.preventDefault();
@@ -77,8 +84,15 @@ const ManualSignUp = () => {
 			photoURL: user?.photoURL,
 			phoneNumber: phoneRef.current.value,
 			isSeller: false,
-			isNewUser: true,
 		});
+		if (yesSeller) {
+			userRef.update({
+				shopName: shopNameRef.current.value,
+				postalCode: postalCodeRef.current.value,
+				address: addressRef.current.value,
+				isSeller: true,
+			});
+		}
 	}
 
 	return (
@@ -90,13 +104,11 @@ const ManualSignUp = () => {
 						<Form.Label>Username</Form.Label>
 						<Form.Control type="text" placeholder="Enter username" ref={usernameRef} required />
 					</Form.Group>
-
 					<Form.Group className="mb-3" controlId="formPassword">
 						<Form.Label>Password</Form.Label>
 						<Form.Control type="password" placeholder="Password" ref={passwordRef} required />
 						<Form.Text> Please enter a password that is at least 6 digits long</Form.Text>
 					</Form.Group>
-
 					<Form.Group className="mb-3" controlId="formConfirmPassword">
 						<Form.Label>Confirm password</Form.Label>
 						<Form.Control
@@ -106,20 +118,18 @@ const ManualSignUp = () => {
 							required
 						/>
 					</Form.Group>
-
 					<Form.Group className="" controlId="formPhoneNumber">
 						<Form.Label>Phone number</Form.Label>
-						<InputGroup className="mb-2">
+						<InputGroup className="mb-3">
 							<InputGroup.Prepend>
 								<InputGroup.Text>+65</InputGroup.Text>
 							</InputGroup.Prepend>
 							<Form.Control type="text" placeholder="Enter your phone number" ref={phoneRef} />
 						</InputGroup>
 					</Form.Group>
-
 					<Form.Group className="" controlId="formEmail">
 						<Form.Label> Email Address </Form.Label>
-						<InputGroup className="mb-2">
+						<InputGroup className="mb-3">
 							<Form.Control
 								type="text"
 								placeholder="Enter your email address"
@@ -128,10 +138,65 @@ const ManualSignUp = () => {
 							/>
 						</InputGroup>
 					</Form.Group>
+					<div className={styles.qnBox}>
+						<h6>Do you want to register as a seller?</h6>
+						<Button
+							variant="outline-primary"
+							onClick={() => {
+								setYesSeller(true);
+								setShowSubmit(true);
+							}}
+							className={styles.replyButtons}
+						>
+							Yes
+						</Button>
 
-					<Button disabled={loading} className="mt-3 w-100" variant="primary" type="submit">
-						Submit
-					</Button>
+						<Button
+							variant="outline-secondary"
+							onClick={() => {
+								setYesSeller(false);
+								setShowSubmit(true);
+							}}
+							className={styles.replyButtons}
+						>
+							No
+						</Button>
+					</div>
+
+					{yesSeller && (
+						<>
+							<Form.Group className="mb-3" controlId="formBakingShopName">
+								<Form.Label>Name of Baking Shop</Form.Label>
+								<Form.Control
+									type="text"
+									placeholder="Enter shop name"
+									ref={shopNameRef}
+									required
+								/>
+							</Form.Group>
+							<Form.Group className="mb-3" controlId="formPostal">
+								<Form.Label>Postal Code</Form.Label>
+								<InputGroup>
+									<InputGroup.Text>Singapore</InputGroup.Text>
+									<Form.Control type="text" placeholder="Enter postal code" ref={postalCodeRef} />
+								</InputGroup>
+							</Form.Group>
+							<Form.Group className="mb-3" controlId="formAddress">
+								<Form.Label>Address</Form.Label>
+								<Form.Control
+									type="text"
+									placeholder=""
+									style={{ height: "4rem" }}
+									ref={addressRef}
+								/>
+							</Form.Group>
+						</>
+					)}
+					{showSubmit && (
+						<Button disabled={loading} className="mt-3 w-100" variant="primary" type="submit">
+							Submit
+						</Button>
+					)}
 				</Form>
 			</Card.Body>
 		</Card>
