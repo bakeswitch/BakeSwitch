5133f78b3d5cefdc57476eaaa1ec2b785562f863
laitzheng <laitzheng@gmail.com>
2021-06-17 11:48:50 +0800

    Fix merge conflicts ProfileSettings

--- src/pages/ProfileSettings.js
+++ src/pages/ProfileSettings.js
@@ -1,21 +1,11 @@
 import React from "react";
-<<<<<<< HEAD
 import Profile from "../components/Profile";
-
-export default function ProfileSettings() {
-	return (
-		<>
-			<Profile />
-		</>
-=======
-import BuyerProfile from "../components/BuyerProfile";
 import styles from "./pages.module.css";
 
 export default function ProfileSettings() {
 	return (
 		<div className={styles.contentBox}>
-			<BuyerProfile />
+			<Profile />
 		</div>
->>>>>>> bakesPage
 	);
 }
