e2c954d9cc6fe740528352d2079372f905188c15
Samuel Yip <samyipsh@gmail.com>
2021-06-22 20:45:52 +0800

    modify bakes, link firebase

--- src/components/SearchResults/Categories.jsx
+++ src/components/SearchResults/Categories.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Card, Row, Col } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
 import styles from "./SearchResults.module.css";
 
 function createCategory(title, imgURL) {
@@ -7,45 +8,36 @@ function createCategory(title, imgURL) {
 }
 
 export default function Categories() {
+    const history = useHistory();
     // const [category, setCategory] = useState();
     // const [categoriesArr, setCategoriesArr] = useState([]);
     const categoriesArr = [
-        createCategory("cake", "https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971552_1280.jpg"),
-        createCategory("cake", "https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971552_1280.jpg"),
-        createCategory("cake", "https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971552_1280.jpg"),
-        createCategory("cake", "https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971552_1280.jpg"),
-        createCategory("cake", "https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971552_1280.jpg"),
-        createCategory("cake", "https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971552_1280.jpg")
+        createCategory("cakes", "https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971552_1280.jpg"),
+        createCategory("cookies", "https://cdn.pixabay.com/photo/2016/11/17/17/37/cookie-1832169_1280.jpg"),
+        createCategory("brownies", "https://cdn.pixabay.com/photo/2014/11/28/08/03/brownie-548591__340.jpg"),
+        createCategory("cupcake", "https://cdn.pixabay.com/photo/2015/03/26/09/39/cupcakes-690040__480.jpg"),
+        createCategory("tarts", "https://images.pexels.com/photos/1157835/pexels-photo-1157835.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"),
+        createCategory("bread", "https://images.pexels.com/photos/2067621/pexels-photo-2067621.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500")
+        //pastry? https://www.snackandbakery.com/ext/resources/images/bakeryproducts.jpg?1432238179?
     ];
     
+    function redirect(cat) {
+        alert(cat + " should be placed into search bar and redirected");
+        // history.push(cat);
+    }
 
     return (
-        <Row xs={1} md={3} className="g-4">
+        <Row xs={1} md={3} className="mb-4 mt-4">
             {categoriesArr.map((category, idx) => (
                 <Col>
-                    <Card>
-                        <Card.Img variant="top" src={category[1]} />
-                        <Card.Body>
+                    <Card onClick={() => redirect(category[0])} className="mb-4">
+                        <Card.Img variant="top" src={category[1]} style={{height:"250px"}} />
+                        <Card.Body style={{backgroundColor: "rgba(233, 233, 233, 0.932)"}}>
                         <Card.Title>{category[0]}</Card.Title>
                         </Card.Body>
                     </Card>
                 </Col>
             ))}
         </Row>
-        // <CardGroup>  
-        //     <Card className="bg-light text-dark" border="secondary" style={{width:"18rem"}}>
-        //         <Card.Img variant="top" src="https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971552_1280.jpg" alt="cake image" />
-        //         <Card.Title>Cake</Card.Title>
-        //     </Card>
-        //     <Card className="bg-light text-dark" border="secondary" style={{width:"18rem"}}>
-        //         <Card.Img variant="top" src="https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971552_1280.jpg" alt="cake image" />
-        //         <Card.Title>Cake</Card.Title>
-        //     </Card>
-        //     <Card className="bg-light text-dark" border="secondary" style={{width:"18rem"}}>
-        //         <Card.Img variant="top" src="https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971552_1280.jpg" alt="cake image" />
-        //         <Card.Title>Cake</Card.Title>
-        //     </Card>
-
-        // </CardGroup>
     )
 }
--- src/components/SearchResults/SearchResults.jsx
+++ src/components/SearchResults/SearchResults.jsx
@@ -1,12 +1,85 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Row, Col, Card } from "react-bootstrap";
 import styles from "./SearchResults.module.css";
+import { db } from "../../config/firebase";
+
+function createColCard(bakeID) {
+    const [bakeObj, setBakeObj] = useState();
+    const [isLoading, setIsLoading] = useState(true);
+
+    const bakeRef =  db.collection("bakes").doc(bakeID);
+    
+    bakeRef.get()       //gets promise
+        .then(snapshot => setBakeObj(snapshot.data()))
+        .catch(err => "setBakeObj error: " + err)
+        .finally(() => setIsLoading(false));
+
+    if (!bakeObj) {
+        return <p>Error loading bakeCard, bakeObj undefined</p>;
+    }
+
+    const { bakeName, bakePriceAndQuantity, bakeStoreUID } = bakeObj;
+    
+    return (
+        <Col>
+            <Card className={styles.card}>
+                <Card.Img className={styles.cardImg} variant="top" src="https://cdn.pixabay.com/photo/2019/12/15/17/06/cookie-4697591__340.jpg"/>
+                <Card.Body className={styles.cardBody}>
+                    <Card.Title>{bakeName}</Card.Title>
+                    <Card.Text>
+                        <p className={styles.itemDescTruncate}>Crunchy outside gooey inside, dark chocolate drizzle</p>
+                        <p>from {bakePriceAndQuantity[0]} dollars onwards<br/>
+                            by <Card.Link>{bakeStoreUID}</Card.Link>
+                        </p>
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </Col>
+    );
+}
 
 export default function SearchResults() {
+    const [bakeIDArr, setBakeIDArr] = useState([]); //array of bakeID strings
+    const [isLoading, setIsLoading] = useState(false);
 
+    function fillBakeIDArr() {
+        setBakeIDArr([]);
+        setIsLoading(true); 
+        
+        const storeRef = db.collection("stores").doc("storeUID");
+        
+        storeRef
+            .get()               // promise of storeDoc
+            .then(snapshot => {
+                alert(snapshot.data().bakesArr.toString());
+                setBakeIDArr(snapshot.data().bakesArr); //PROBLEMLINE
+            }) //stores bakeIDs in bakeArr
+            .catch(err => alert("err unable to set bakeArr: " + err))
+            .finally(() => setIsLoading(false));
+    }
+
+
+
+    const itemDetails = ({
+        title:"Chocolate Cookies",
+        price: "$12",
+        sellerName: "seller123"
+    });
+   
+    useEffect(() => {
+        // fillBakeIDArr();
+        // setIsLoading(false);
+    }, []);
 
     return (
-        <>
-            <div>testsearchresults</div>
-        </>
+        !isLoading && (
+            <Row xs={2} md={4} className="mb-4 mt-4"> 
+                {/* <Col>{JSON.stringify(bakeDetailsArr[0])}</Col>
+                <Col>bakedocarr length: {bakeDocArr.length}</Col>
+                <Col>{JSON.stringify(bakeArr)}</Col> */}
+                
+                {[1,2,3,4,5,6,7,8].map(bakeID => createColCard("bakeUID"))}; 
+            </Row>
+        )
     )
 }
