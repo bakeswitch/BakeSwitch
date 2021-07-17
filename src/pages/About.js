4e053bc3fe8313999329b21ab07efb0733b56500
laitzheng <laitzheng@gmail.com>
2021-07-17 09:01:58 +0800

    Add div for styling in About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -44,33 +44,37 @@ export default function About() {
 
 	return (
 		<div className={styles.contentBox}>
-			<h3 id="our-story" className="mt-5 mb-4">
-				Our Story
-			</h3>
-			<p style={{ textAlign: "left" }}>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-				labore et dolore magna aliqua. Velit egestas dui id ornare arcu odio ut sem nulla. Mauris
-				augue neque gravida in. Neque laoreet suspendisse interdum consectetur libero id faucibus.
-				Mauris sit amet massa vitae. Ac tortor dignissim convallis aenean et. Dolor morbi non arcu
-				risus quis varius quam.
-			</p>
-			<p style={{ textAlign: "left" }}>
-				Fermentum iaculis eu non diam. Viverra maecenas accumsan lacus vel facilisis volutpat est
-				velit egestas. Ut venenatis tellus in metus vulputate eu scelerisque. Aliquam etiam erat
-				velit scelerisque in. Sapien nec sagittis aliquam malesuada bibendum arcu. Tristique
-				sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula. Pharetra et ultrices
-				neque ornare aenean euismod elementum nisi quis. Arcu cursus euismod quis viverra nibh cras
-				pulvinar mattis. Habitant morbi tristique senectus et netus et malesuada fames. Facilisis
-				sed odio morbi quis commodo. Gravida neque convallis a cras semper auctor neque vitae.
-			</p>
-
-			<h3 id="faq" className="mt-5 mb-4">
-				FAQ
-			</h3>
-			<div className={styles.questionBox}>
-				{questionList.map((questionItem, index) => (
-					<QuestionBlock question={questionItem.qn} answer={questionItem.ans} key={index} />
-				))}
+			<div className={styles.ourStory}>
+				<h3 id="our-story" className="mt-5 mb-4">
+					Our Story
+				</h3>
+				<p style={{ textAlign: "left" }}>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+					ut labore et dolore magna aliqua. Velit egestas dui id ornare arcu odio ut sem nulla.
+					Mauris augue neque gravida in. Neque laoreet suspendisse interdum consectetur libero id
+					faucibus. Mauris sit amet massa vitae. Ac tortor dignissim convallis aenean et. Dolor
+					morbi non arcu risus quis varius quam.
+				</p>
+				<p style={{ textAlign: "left" }}>
+					Fermentum iaculis eu non diam. Viverra maecenas accumsan lacus vel facilisis volutpat est
+					velit egestas. Ut venenatis tellus in metus vulputate eu scelerisque. Aliquam etiam erat
+					velit scelerisque in. Sapien nec sagittis aliquam malesuada bibendum arcu. Tristique
+					sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula. Pharetra et ultrices
+					neque ornare aenean euismod elementum nisi quis. Arcu cursus euismod quis viverra nibh
+					cras pulvinar mattis. Habitant morbi tristique senectus et netus et malesuada fames.
+					Facilisis sed odio morbi quis commodo. Gravida neque convallis a cras semper auctor neque
+					vitae.
+				</p>
+			</div>
+			<div className={styles.faq}>
+				<h3 id="faq" className="mt-5 mb-4">
+					FAQ
+				</h3>
+				<div className={styles.questionBox}>
+					{questionList.map((questionItem, index) => (
+						<QuestionBlock question={questionItem.qn} answer={questionItem.ans} key={index} />
+					))}
+				</div>
 			</div>
 
 			<h3 id="contact" className="mt-5 mb-4">
