d128af8057f3f88f735c765e88ba55cabcf88223
Samuel Yip <samyipsh@gmail.com>
2021-07-20 20:39:40 +0800

    add framework for userOrders, managed to populate each docID

---
+++ src/components/UserOrders/index.js
@@ -0,0 +1,2 @@
+import UserOrders from "./UserOrders";
+export default UserOrders;
--- src/pages/OrderCart.js
+++ src/pages/OrderCart.js
@@ -1,6 +1,11 @@
 import React from "react";
 import styles from "./pages.module.css";
+import UserOrders from "../components/UserOrders/";
 
 export default function OrderCart() {
-	return <div className={styles.contentBox}></div>;
+	return (
+		<div className={styles.contentBox}>
+			<UserOrders />
+		</div>
+	);
 }
