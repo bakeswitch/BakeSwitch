2de1a289f48ff509fa44c64024c7e0fd154aaba9
Samuel Yip <samyipsh@gmail.com>
2021-07-21 15:18:39 +0800

    modify userOrderCard and userOrders

--- src/components/UserOrderCard/UserOrderCard.jsx
+++ src/components/UserOrderCard/UserOrderCard.jsx
@@ -1,48 +1,238 @@
 import React, { useState, useEffect } from "react";
-import { Card, Image, Button } from "react-bootstrap";
-import styles from "./BakersPage.module.css";
+import { Card, Image, Button, ListGroup, Col, Row, Breadcrumb } from "react-bootstrap";
+import styles from "./UserOrderCard.module.css";
 import { db } from "../../config/firebase";
 import { useHistory } from "react-router-dom";
+import ErrorCard from "../helperComponents/ErrorCard";
 
-export default function OrderCard(props) {
-	const [storeRec, setStoreRec] = useState();
-	const [loading, setLoading] = useState(true);
-	const history = useHistory();
-
-	useEffect(
-		() =>
-			db
-				.collection("stores")
-				.doc(props.storeID)
-				.get()
-				.then((snapshot) => setStoreRec(snapshot.data()))
-				.then(() => setLoading(false)),
-		[]
-	);
+function UserOrderCard(props) {
+	const { modeOfTransfer = "defaultModeOfTransfer",
+			bakeName = "defaultBakeName",
+			bakeSet = "defaultBakeSet",
+			qty = 1,
+			unitprice = 0,
+			remarks = "defaultRemarks",
+			bakePhotoURL = "https://cdn.pixabay.com/photo/2016/10/12/22/40/bar-1736191_1280.jpg" } = props
 
 	function handleClick() {
-        alert('ive been clicked');
+		alert('ive been clicked');
 		// history.push(`/bakerProfile/${props.storeID}`);
 	}
 
 	return (
-		!loading && (
-			<div className={styles.orderBox}>
-				<Image
-					src= 'https://media.istockphoto.com/photos/different-flavours-cupcakes-picture-id1251444635?b=1&k=6&m=1251444635&s=170667a&w=0&h=jrfRMjs40vlFO9oKNsSrelDcMyRF3MIF-VuXLBUoD3M='
-					alt="Bake picture"
-					roundedCircle
-					className={styles.bakeImg}
+		// <Card key={"card_" + bakeID} className={styles.card} onClick={handleOnClick}>
+		// 	<Card.Img key={"img_" + bakeID}
+		// 		className={styles.cardImg}
+		// 		variant="top"
+		// 		src={bakePhotoURL}
+		// 	/>
+		// 	<Card.Body key={"body_" + bakeID} className={styles.cardBody}>
+		// 		<Card.Title key={"title_" + bakeID} className={styles.max2Lines}>{bakeName}</Card.Title>
+		// 		<Card.Text key={"desc_" + bakeID} className={styles.max3Lines}>
+		// 				{bakeDesc}
+		// 		</Card.Text>
+		// 		<Card.Text key={"footer_" + bakeID} className={styles.cardFooter}>
+		// 			from S${orderedPriceAndQtyArr[0][0]} dollars onwards
+		// 			<br />
+		// 			by <Card.Link key={"store_" + bakeID}>{storeID}</Card.Link>
+		// 		</Card.Text>
+		// 	</Card.Body>
+		// </Card>
+	
+
+		<ListGroup.Item>
+			<Row>
+				<Col xs={2}>
+					<Image
+						variant = "left"
+						src= {bakePhotoURL}
+						alt="Bake picture"
+						// roundedCircle
+						className={styles.bakeImg}
+					/>
+				</Col>
+				<Col md={10}>
+					<Card className={styles.orderCard}>
+						<Card.Header as="h6" onClick={handleClick}>{bakeName}</Card.Header>
+						<Card.Body className={styles.orderDesc}>
+							<Row>
+								<Col xs="auto">
+									<Card.Text className={styles.priceQtyAndMode}>
+										<h6>{qty} x ["{bakeSet}"] set</h6>
+										{modeOfTransfer}<br/>
+									</Card.Text>
+
+								</Col>
+								<Col xs={7} styles={{justifyContent:"center"}}>
+									"{remarks}""
+								</Col>
+								<Col className="ms-auto" as="h2" xs={2}>
+										${unitprice}
+								</Col>
+
+							</Row>
+						</Card.Body>
+					</Card>
+				</Col>
+			</Row>
+		</ListGroup.Item>
+	);
+}
+
+// function orderHeader(props) {
+// 	return (
+		
+// 	)
+// }
+export function LoadUserStoreOrders(props) {
+	const { userID, storeID } = props;
+	const [userOrderData, setUserOrderData] = useState();
+	const [isLoading, setIsLoading] = useState(true);
+	const history = useHistory();
+	const userOrderRef = db.collection("users").doc(userID)
+							.collection("user-orders").doc(storeID);
+	
+	// if userOrderRef has no doc?
+	useEffect(
+		() => fillUserOrderData(),
+		[]
+	);
+	
+	function fillUserOrderData() {
+		userOrderRef.get()
+			.then((snapshot) => {
+				setIsLoading(true);
+				if (snapshot && snapshot.exists) {
+					setUserOrderData(snapshot.data());
+				} else {
+					alert('snapshot doesnt exist');
+				}
+			}).catch(err => alert("setUserOrderObj error: " + err))
+			.finally(() => setIsLoading(false));
+	}
+
+	if (!userOrderData) {
+		return ErrorCard("no user order data");
+	}
+
+	const { storeName = "defaultStoreName",
+			totalCost = "defaultTotalCost",
+			orderObj  = "defaultOrderObj"} = userOrderData;
+	const { modeOfTransfer 	= "defaultModeOfTransfer",
+			bakeName 		= "defaultBakeName",
+			bakeSet 		= "defaultBakeSet",
+			qty 			= 1,
+			unitprice	 	= 0,
+			remarks 		= "defaultRemarks",
+			bakePhotoURL 	= "https://cdn.pixabay.com/photo/2016/10/12/22/40/bar-1736191_1280.jpg" } = orderObj[2];
+	
+	return (
+		!isLoading && (
+			<ListGroup variant="flush">
+				<ListGroup.Item>
+					<Row>
+						<Col xs={2}> {storeName} </Col>
+						{/* <Col>totalCost: ${totalCost}</Col> */}
+						<Col className="ms-auto" xs={3}>
+							<a className={{color: "blue"}} onClick={() => alert("throw modal")}>
+								Generate Order Text
+							</a>
+						</Col>
+					</Row>
+				</ListGroup.Item>
+				<UserOrderCard 
+					modeOfTransfer = {modeOfTransfer == "collection" ? "-Collection-": "-Delivery-"}
+					bakeName = {bakeName}
+					bakeSet = {bakeSet}
+					qty = {qty}
+					unitprice = {unitprice}
+					remarks = {remarks}
+					bakePhotoURL = {bakePhotoURL}
 				/>
-				<Card className={styles.orderCard}>
-					<Button variant="light" onClick={handleClick}>
-						<Card.Header as="h5">lala</Card.Header>
-					</Button>
-					<Card.Body className={styles.orderDesc}>
-						<Card.Text>lalal</Card.Text>
-					</Card.Body>
-				</Card>
-			</div>
+			</ListGroup>
 		)
 	);
 }
+
+/*
+export function DisplayBakeCard(props) {
+	const bakeID = props.bakeID;
+	const [bakeData, setBakeData] = useState();
+	const [orderedPriceAndQtyArr, setOrderedPriceAndQtyArr] = useState([["default_price","default_qty"]]);
+	const [isLoading, setIsLoading] = useState(false);
+
+	if (bakeID == "") {
+		return "Don't load";
+	}
+
+	const bakeRef = db.collection("bakes").doc(bakeID);
+	const history = useHistory();
+
+	
+	function fillBakeData() {
+		bakeRef.get()
+			.then((snapshot) => {
+				if (snapshot && snapshot.exists) {
+					setBakeData(snapshot.data());
+					// alert("bakeData set");
+				} else {
+					alert('snapshot doesnt exist');
+				}
+			}).catch((err) => alert("setBakeObj error: " + err));
+	}
+
+	function getRealTimeUpdates() {
+		bakeRef.onSnapshot((snapshot) => {
+			if (snapshot && snapshot.exists) {
+				// alert(JSON.stringify(snapshot.data())); //runs here
+				const orderedPnQArr = orderPriceAndQtyArr(snapshot.data());
+				// alert(JSON.stringify(orderedPnQ)); //Doesnt run here
+				setOrderedPriceAndQtyArr(orderedPnQArr);
+			} else {
+				alert("snapshot doesnt exist for realtime update");
+			}
+		})
+	}
+	
+	useEffect(() => {
+		try {
+			setIsLoading(true);
+			fillBakeData();
+			getRealTimeUpdates();
+		} finally {
+			setIsLoading(false);
+		}
+		// return (() => {
+		// 	setBakeData([]);
+		// 	setIsLoading(false);
+		// })
+	},[]);
+	
+	if (!bakeData) { 
+		return ErrorCard("no bake data") 
+	}
+	
+	//pass in default values in case can't read fields
+	const { bakeName 		= 'default_bake_name', 
+			storeID  		= 'default_store_id',
+			bakeDesc 		= 'default_bake_desc',
+			bakePhotoURL 	= 'default_bake_photo' } = bakeData;
+	
+	function handleOnClick() {
+		history.push(`/bake-product/${bakeID}`);
+	}
+	
+	return !isLoading && ( 
+		<BakeCard 
+			key = {"displayBakeCard_" + bakeID}
+			bakeID = {bakeID}
+			handleOnClick = {handleOnClick}
+			bakePhotoURL = {bakePhotoURL}
+			bakeName = {bakeName}
+			bakeDesc = {bakeDesc}
+			orderedPriceAndQtyArr = {orderedPriceAndQtyArr}
+			storeID = {storeID}
+		/>
+	);
+}
+*/
--- src/components/UserOrders/UserOrders.jsx
+++ src/components/UserOrders/UserOrders.jsx
@@ -1,15 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { Card } from "react-bootstrap";
+import { LoadUserStoreOrders } from "../UserOrderCard/UserOrderCard.jsx";
 import { useAuth } from "../../contexts/AuthContext";
 import { db } from "../../config/firebase";
 
-function UserOrderPerStore(props) {
-    const [userOrderData, setUserOrderData] = useState();
-    const { storeID } = props;
-    const userOrderRef = db.collection("users").doc(userID).collection("user-orders").doc(storeID);
-
-
-}
 
 export default function UserOrders() {
 	// const [orderedPriceAndQtyArr, setOrderedPriceAndQtyArr] = useState([["default_price","default_qty"]]);
@@ -31,14 +25,14 @@ export default function UserOrders() {
         })
     },[]);
         
+    //for each user, take out all stores which he has ordered from & populate array to reference later
     function fillStoreIDArr(userID) {
-
         const userOrdersRef = db.collection("users").doc(userID).collection("user-orders");
         userOrdersRef.get()    
             .then((querySnapshot) => {
                 querySnapshot.forEach((doc) => {
                     // doc.data() is never undefined for query doc snapshots
-                    alert("run inside foreach")
+                    // alert("run inside foreach")
                     setStoreIDArr((prevArr) => {
                         return [...prevArr, doc.id];
                     });
@@ -52,91 +46,12 @@ export default function UserOrders() {
 
     return ( !isLoading && 
         <>
-            <Card>
-                storeID: {JSON.stringify(storeIDArr)}
-            </Card>
+			{/* storeID: {JSON.stringify(storeIDArr)} */}
+			<LoadUserStoreOrders 
+				userID = "Wq33g2sv1Qbkj3RvxARGS419oNC2"
+				storeID = "store_0001"
+			/>
         </>
     )
 }
-/*
-export function DisplayBakeCard(props) {
-	const bakeID = props.bakeID;
-	const [bakeData, setBakeData] = useState();
-	const [orderedPriceAndQtyArr, setOrderedPriceAndQtyArr] = useState([["default_price","default_qty"]]);
-	const [isLoading, setIsLoading] = useState(false);
 
-	if (bakeID == "") {
-		return "Don't load";
-	}
-
-	const bakeRef = db.collection("bakes").doc(bakeID);
-	const history = useHistory();
-
-	
-	function fillBakeData() {
-		bakeRef.get()
-			.then((snapshot) => {
-				if (snapshot && snapshot.exists) {
-					setBakeData(snapshot.data());
-					// alert("bakeData set");
-				} else {
-					alert('snapshot doesnt exist');
-				}
-			}).catch((err) => alert("setBakeObj error: " + err));
-	}
-
-	function getRealTimeUpdates() {
-		bakeRef.onSnapshot((snapshot) => {
-			if (snapshot && snapshot.exists) {
-				// alert(JSON.stringify(snapshot.data())); //runs here
-				const orderedPnQArr = orderPriceAndQtyArr(snapshot.data());
-				// alert(JSON.stringify(orderedPnQ)); //Doesnt run here
-				setOrderedPriceAndQtyArr(orderedPnQArr);
-			} else {
-				alert("snapshot doesnt exist for realtime update");
-			}
-		})
-	}
-	
-	useEffect(() => {
-		try {
-			setIsLoading(true);
-			fillBakeData();
-			getRealTimeUpdates();
-		} finally {
-			setIsLoading(false);
-		}
-		// return (() => {
-		// 	setBakeData([]);
-		// 	setIsLoading(false);
-		// })
-	},[]);
-	
-	if (!bakeData) { 
-		return ErrorCard("no bake data") 
-	}
-	
-	//pass in default values in case can't read fields
-	const { bakeName 		= 'default_bake_name', 
-			storeID  		= 'default_store_id',
-			bakeDesc 		= 'default_bake_desc',
-			bakePhotoURL 	= 'default_bake_photo' } = bakeData;
-	
-	function handleOnClick() {
-		history.push(`/bake-product/${bakeID}`);
-	}
-	
-	return !isLoading && ( 
-		<BakeCard 
-			key = {"displayBakeCard_" + bakeID}
-			bakeID = {bakeID}
-			handleOnClick = {handleOnClick}
-			bakePhotoURL = {bakePhotoURL}
-			bakeName = {bakeName}
-			bakeDesc = {bakeDesc}
-			orderedPriceAndQtyArr = {orderedPriceAndQtyArr}
-			storeID = {storeID}
-		/>
-	);
-}
-*/
