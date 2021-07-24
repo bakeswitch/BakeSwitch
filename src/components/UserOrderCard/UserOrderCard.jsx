fa308a5c53bb5fac19cdf098efaf9a5c48093389
laitzheng <laitzheng@gmail.com>
2021-07-24 21:24:27 +0800

    Fix console warnings for UserOrders

--- src/components/UserOrderCard/UserOrderCard.jsx
+++ src/components/UserOrderCard/UserOrderCard.jsx
@@ -9,39 +9,54 @@ import { AiOutlineCloseCircle } from "react-icons/ai";
 import { delOrderFromUserOrders, delUserOrderDoc } from "../../helperFunctions/handleDataFunctions";
 
 export default function UserOrderCard(props) {
-	const { storeID = 'defaultStoreID',
-			uid = 'defaultUID',
-			bakeID = 'defaultBakeID',
-			storeName = 'defaultStoreName',
-			modeOfTransfer = "defaultModeOfTransfer",
-			bakeName = "defaultBakeName",
-			bakeSet = "defaultBakeSet",
-			qty = 1,
-			unitPrice = 0,
-			remarks = "defaultRemarks",
-			bakePhotoURL = "https://cdn.pixabay.com/photo/2016/10/12/22/40/bar-1736191_1280.jpg",
-			numOfStoreOrders = "defaultNumOfOrdersInStore",
-			setNumOfStoreOrders = 'defaultSetFunction'} = props;
+	const {
+		storeID = "defaultStoreID",
+		uid = "defaultUID",
+		bakeID = "defaultBakeID",
+		storeName = "defaultStoreName",
+		modeOfTransfer = "defaultModeOfTransfer",
+		bakeName = "defaultBakeName",
+		bakeSet = "defaultBakeSet",
+		qty = 1,
+		unitPrice = 0,
+		remarks = "defaultRemarks",
+		bakePhotoURL = "https://cdn.pixabay.com/photo/2016/10/12/22/40/bar-1736191_1280.jpg",
+		numOfStoreOrders = "defaultNumOfOrdersInStore",
+		setNumOfStoreOrders = "defaultSetFunction",
+	} = props;
 	const history = useHistory();
 
 	const displayModeOfTransfer = (modeOfTransfer) =>
-		 modeOfTransfer == "collection" ? "-Collection-" : "-Delivery-";
+		modeOfTransfer == "collection" ? "-Collection-" : "-Delivery-";
 
 	function handleBakeClick() {
 		history.push(`bake-product/${bakeID}`);
 	}
 
 	function handleDelete() {
-		setNumOfStoreOrders(prvNum => prvNum - 1);
+		setNumOfStoreOrders((prvNum) => prvNum - 1);
 		// alert("numOfStoreOrders: " + numOfStoreOrders);
-        const userOrdersRef = db
-		.collection("users").doc(uid) //samyipsh@gmail.com acct
-		.collection("user-orders").doc(storeID); //modify name (storeID)
+		const userOrdersRef = db
+			.collection("users")
+			.doc(uid) //samyipsh@gmail.com acct
+			.collection("user-orders")
+			.doc(storeID); //modify name (storeID)
 		if (numOfStoreOrders == 1) {
 			// alert('delete store');
 			delUserOrderDoc(userOrdersRef);
 		} else if (numOfStoreOrders > 1) {
-			delOrderFromUserOrders(userOrdersRef, storeName, qty, bakeSet, bakeName, unitPrice, modeOfTransfer, remarks, bakePhotoURL, bakeID)
+			delOrderFromUserOrders(
+				userOrdersRef,
+				storeName,
+				qty,
+				bakeSet,
+				bakeName,
+				unitPrice,
+				modeOfTransfer,
+				remarks,
+				bakePhotoURL,
+				bakeID
+			);
 		}
 	}
 
@@ -49,33 +64,28 @@ export default function UserOrderCard(props) {
 		<ListGroup.Item>
 			<Row>
 				<Col xs="auto">
-					<Image
-						variant = "left"
-						src= {bakePhotoURL}
-						alt="Bake picture"
-						className={styles.bakeImg}
-					/>
+					<Image variant="left" src={bakePhotoURL} alt="Bake picture" className={styles.bakeImg} />
 				</Col>
 				<Col className={styles.colWithCard}>
 					<Card className={styles.orderCard}>
 						<Card.Header as="h6" onClick={handleBakeClick}>
-							{bakeName}  
+							{bakeName}
 						</Card.Header>
 						<Card.Body className={styles.orderDesc}>
 							<Row>
 								<Col xs="auto" className="me-auto">
 									<Card.Text className={styles.priceQtyAndMode}>
-										<h6>{qty} x ["{bakeSet}"]</h6>
-										{displayModeOfTransfer(modeOfTransfer)}<br/>
+										<h6>
+											{qty} x ["{bakeSet}"]
+										</h6>
+										{displayModeOfTransfer(modeOfTransfer)}
+										<br />
 									</Card.Text>
 								</Col>
-								<Col className={styles.remarks}>
-								 	{remarks} 
-								</Col>
+								<Col className={styles.remarks}>{remarks}</Col>
 								<Col className="ms-auto" as="h6" xs={2}>
 									${unitPrice}
 								</Col>
-
 							</Row>
 						</Card.Body>
 					</Card>
@@ -89,7 +99,7 @@ export default function UserOrderCard(props) {
 }
 
 // function ModalPopUp(props) {
-// 	const { headerText = "defaultHeader", 
+// 	const { headerText = "defaultHeader",
 // 			bodyText = "defaultBody",
 // 			handleClose = () => alert("defaultHandleClose"),
 // 			handleShow = () => alert("defaultHandleShow"),
@@ -102,7 +112,7 @@ export default function UserOrderCard(props) {
 // 				<Modal.Title>{headerText}</Modal.Title>
 // 			</Modal.Header>
 // 			<Modal.Body style={{fontSize:"small"}}>
-// 				{`why doesnt \n 
+// 				{`why doesnt \n
 // 				new lines show`}
 // 				{/* {bodyText} */}
 // 			</Modal.Body>
@@ -118,4 +128,3 @@ export default function UserOrderCard(props) {
 // 	  </>
 // 	);
 //   }
-  
