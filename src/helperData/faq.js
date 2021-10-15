1c5afa14bef5ef34c7d06ac956800097663822c1
ongyichong <yichong96@gmail.com>
2021-10-15 15:37:09 +0800

    add button link to feedback and step by step instruction on upload bake pictures

--- src/helperData/faq.js
+++ src/helperData/faq.js
@@ -3,7 +3,11 @@ export const questionList = [
         qn: "How do I add local pictures to my Listed Products?",
         ans: <p>As our websites only support photo URL, you may upload all the bake pictures to <a href='https://postimages.org'>PostImages</a>,<br/>
         It is a really great and easy site to have a online gallery of all your pictures!<br/>
-        From there, simply click the share icon and copy the direct link over to our site :)</p>
+        From there, simply click the share icon and copy the direct link over to our site :)
+        <br></br>
+        <br></br>
+        <a href="https://docs.google.com/document/d/1jtxoQbDKcIL_yHTxAKcrzfNpR4JoIhuV/edit?usp=sharing&ouid=105976115056790916953&rtpof=true&sd=true" target="_blank" rel="noopener noreferrer">Step by step instruction guide on how to upload bake pictures
+        </a></p>
         ,
     }
     // {
